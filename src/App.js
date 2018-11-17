d40cd82a875a3bdb028d08d8eb1c4440d277d045
Dubois Matthieu <dubois.matthieu@live.be>
2018-11-17 22:25:28 +0100

    Ajout de sampleText, création du template de base

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+              className="form-control" 
+              rows="35">
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <h1>Résultats</h1>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
