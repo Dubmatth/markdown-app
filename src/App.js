9ac076eb4c1a2b817bdd8afda698eb455346d401
Dubois Matthieu <dubois.matthieu@live.be>
2018-11-17 22:38:15 +0100

    ajout function onChange

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { sampleText } from './sampleText';
 
 class App extends Component {
+
+  state = {
+    userInput: sampleText
+  } 
+
+  onChange = event => {
+    const userInput = event.target.value
+    this.setState({ userInput })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
             <textarea 
+              value={this.state.userInput}
+              onChange={this.onChange}
               className="form-control" 
               rows="35">
             </textarea>
           </div>
           <div className="col-sm-6">
-            <h1>Résultats</h1>
+            <div>
+              {this.state.userInput}
+            </div>
           </div>
         </div>
       </div>
